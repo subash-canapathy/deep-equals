35a77c65c67ee106fd85cf2e53cd90fb6ba7f45e
jdereg <jdereg@2cdccfab-c21f-54c2-91c2-4e02620a6753>
2011-04-14 18:02:54 +0000

    initial put

---
+++ deep-equals/java/com/cedarsoftware/util/DeepEquals.java
@@ -0,0 +1,318 @@
+package com.gaic.uwd.core.util;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Deeply compare two (2) objects.  This method will call any overridden
+ * equals() methods if they exist.  If not, it will then proceed to do a
+ * field-by-field comparison, and when a non-primitive field is encountered,
+ * recursively continue the deep comparison. When an array is found, it will
+ * also ensure that the array contents are deeply equal, not requiring the 
+ * array instance (container) to be identical.  This method will successfully 
+ * compare object graphs that have cycles (A->B->C->A).  There is no need to
+ * ever use the Arrays.deepEquals() method as this is a true and more effective
+ * super set.
+ *  
+ * @author John DeRegnaucourt
+ */
+public class DeepEquals
+{
+    private static final Map<Class, Boolean> _customEquals = new ConcurrentHashMap<Class, Boolean>();
+    private static final Map<Class, Boolean> _customHash = new ConcurrentHashMap<Class, Boolean>();
+    private static final Map<Class, Collection<Field>> _reflectedFields = new ConcurrentHashMap<Class, Collection<Field>>();
+    
+    private static class DualKey
+    {
+        private Object _key1;
+        private Object _key2;
+        
+        private DualKey() { }
+        private DualKey(Object k1, Object k2)
+        {
+            _key1 = k1;
+            _key2 = k2;
+        }
+        
+        public boolean equals(Object other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            
+            if (!(other instanceof DualKey))
+            {
+                return false;
+            }
+            
+            DualKey that = (DualKey) other;
+            boolean key1Equal = false;
+            if (_key1 == null || that._key1 == null)
+            {
+                key1Equal = _key1 == that._key1;
+            }
+            else
+            {
+                key1Equal = _key1.equals(that._key1);
+            }
+            
+            if (!key1Equal)
+            {
+                return false;
+            }
+            
+            boolean key2Equal = false;
+            if (_key2 == null || that._key2 == null)
+            {
+                key2Equal = _key2 == that._key2;
+            }
+            else
+            {
+                key2Equal = _key2.equals(that._key2);
+            }
+            
+            return key2Equal;
+        }
+        
+        public int hashCode()
+        {
+            int h1 = _key1 != null ? _key1.hashCode() : 0;
+            int h2 = _key2 != null ? _key2.hashCode() : 0;
+            return h1 + h2;
+        }
+    }
+    
+    public static boolean deepEquals(Object a, Object b)
+    {
+        Set visited = new HashSet<DualKey>();
+        LinkedList<DualKey> stack = new LinkedList<DualKey>();
+        stack.addFirst(new DualKey(a, b));
+
+        while (!stack.isEmpty())
+        {
+            DualKey dualKey = stack.removeFirst();        
+            visited.add(dualKey);
+            
+            if (dualKey._key1 == null || dualKey._key2 == null)
+            {
+                if (dualKey._key1 != dualKey._key2)
+                {
+                    return false;
+                }
+                continue;
+            }
+                            
+            if (!dualKey._key1.getClass().equals(dualKey._key2.getClass()))
+            {
+                return false;
+            }
+            
+            if (dualKey._key1.getClass().isArray())
+            {
+                int len = Array.getLength(dualKey._key1);
+                if (len != Array.getLength(dualKey._key2))
+                {
+                    return false;
+                }
+                    
+                for (int i = 0; i < len; i++)
+                {
+                    DualKey dk = new DualKey();
+                    dk._key1 = Array.get(dualKey._key1, i);
+                    dk._key2 = Array.get(dualKey._key2, i);
+                    
+                    if (!visited.contains(dk))
+                    {
+                        stack.addFirst(dk);
+                    }
+                }
+                continue;
+            }
+            
+            if (hasCustomEquals(dualKey._key1.getClass()))
+            {
+                if (!dualKey._key1.equals(dualKey._key2))
+                {
+                    return false;
+                }
+                continue;
+            }        
+            
+            Collection<Field> fields = getDeepDeclaredFields(dualKey._key1.getClass());               
+            
+            for (Field field : fields)
+            {
+                try
+                {
+                    DualKey dk = new DualKey();
+                    dk._key1 = field.get(dualKey._key1);
+                    dk._key2 = field.get(dualKey._key2);
+                    if (!visited.contains(dk))
+                    {
+                        stack.addFirst(dk);
+                    }
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+            }
+        }
+
+        return true;
+    }
+    
+    public static boolean hasCustomEquals(Class c)
+    {        
+        Class origClass = c;
+        if (_customEquals.containsKey(c))
+        {
+            return _customEquals.get(c);
+        }
+
+        while (!Object.class.equals(c))
+        {
+            try
+            {
+                c.getDeclaredMethod("equals", Object.class);
+                _customEquals.put(origClass, true);
+                return true;
+            }
+            catch (Exception ignored) { }
+            c = c.getSuperclass();
+        }
+        _customEquals.put(origClass, false);
+        return false;
+    }
+
+    public static int deepHashCode(Object obj)
+    {
+        Set visited = new HashSet();
+        LinkedList<Object> stack = new LinkedList<Object>();
+        stack.addFirst(obj);
+        int hash = 0;
+
+        while (!stack.isEmpty())
+        {
+            obj = stack.removeFirst();
+            if (obj == null || visited.contains(obj))
+            {
+                continue;
+            }
+            
+            visited.add(obj);
+            
+            if (obj.getClass().isArray())
+            {
+                int len = Array.getLength(obj);
+                for (int i = 0; i < len; i++)
+                {        
+                    stack.addFirst(Array.get(obj, i));
+                }
+                continue;
+            }
+            
+            if (hasCustomHashCode(obj.getClass()))
+            {   // A real hashCode() method exists, call it.
+                hash += obj.hashCode();
+                continue;
+            }
+            
+            Collection<Field> fields = getDeepDeclaredFields(obj.getClass());
+            for (Field field : fields)
+            {
+                try
+                {           
+                    stack.addFirst(field.get(obj));
+                }
+                catch (Exception ignored) { }
+            }
+        }
+        return hash;        
+    }
+        
+    public static boolean hasCustomHashCode(Class c)
+    {   
+        Class origClass = c;
+        if (_customHash.containsKey(c))
+        {
+            return _customHash.get(c);
+        }
+        
+        while (!Object.class.equals(c))
+        {
+            try
+            {
+                c.getDeclaredMethod("hashCode");
+                _customHash.put(origClass, true);
+                return true;
+            }
+            catch (Exception ignored) { }
+            c = c.getSuperclass();
+        }
+        _customHash.put(origClass, false);
+        return false;
+    }
+    
+    /**
+     * @param c Class instance
+     * @return Collection of only the fields in the passed in class
+     * that would need further processing (reference fields).  This
+     * makes field traversal on a class faster as it does not need to
+     * continually process known fields like primitives.
+     */
+    public static Collection<Field> getDeepDeclaredFields(Class c)
+    {
+        if (_reflectedFields.containsKey(c))
+        {
+            return _reflectedFields.get(c);
+        }
+        Collection<Field> fields = new ArrayList<Field>();
+        Class curr = c;
+        
+        while (curr != null)
+        {
+            try 
+            {
+                Field[] local;
+                local = curr.getDeclaredFields();
+                
+                for (Field field : local)
+                {
+                    if (!field.isAccessible())
+                    {
+                        try 
+                        {
+                            field.setAccessible(true);
+                        }
+                        catch (Exception ignored) { }
+                    }
+                    
+                    if (!Modifier.isStatic(field.getModifiers()) && !field.getName().startsWith("this$"))
+                    {   // speed up: do not count static fields, not go back up to enclosing object in nested case    
+                        fields.add(field);
+                    }                                      
+                }               
+            }
+            catch (ThreadDeath t)
+            {
+                throw t;
+            }
+            catch (Throwable ignored)
+            { }
+
+            curr = curr.getSuperclass();
+        }
+        _reflectedFields.put(c, fields);
+        return fields;
+    }            
+}
---
+++ deep-equals/test/com/cedarsoftware/util/DeepEqualsTest.java
@@ -0,0 +1,282 @@
+package com.gaic.uwd.core.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.gaic.uwd.core.util.test.AbstractBaseTestSpringImpl;
+
+/**
+ * Test for DeepEquals (equals() and hashCode())
+ *
+ * @author John DeRegnaucourt (jdereg@gmail.com)
+ *         <br/>
+ *         Copyright [2010] John DeRegnaucourt
+ *         <br/><br/>
+ *         Licensed under the Apache License, Version 2.0 (the "License");
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br/><br/>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br/><br/>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
+public class DeepEqualsTest extends AbstractBaseTestSpringImpl 
+{
+    @Override
+    public String[] getConfigLocations()
+    {
+        String [] configLocations = {"classpath:spring/conf/uwd-core-util-test.xml", "classpath:spring/conf/uwd-core-util-test-optional.xml"};
+        return configLocations;
+    }
+    
+    private class Person
+    {
+        String first;
+        String last;
+        Pet pet;
+    }
+    
+    private class Pet
+    {
+        String name;
+        String type;
+    }
+    
+    private class ArrayClass
+    {
+        String name;
+        Object[] items;
+    }
+    
+    private class Cycle
+    {
+        String value;
+        Object next;
+    }      
+    
+    private class FixedHierarchy
+    {
+        String value;
+        Object child1;
+        Object child2;
+        Object child3;
+    }
+    
+    private class SmartPet extends Pet
+    {
+        // Pathological equals!!! Intentionally wrong to prove that it is called.
+        public boolean equals(Object o)
+        {
+            if (o == null || !(o instanceof Pet))
+            {
+                return false;
+            }
+            
+            Pet that = (Pet) o;
+            boolean nameEquals;
+            if (name == null || that.name == null)
+            {
+                nameEquals = name != that.name;
+            }
+            else
+            {
+                nameEquals = !name.equals(that.name);
+            }
+            
+            if (!nameEquals)
+            {
+                return false;
+            }
+            boolean typeEquals;
+            if (type == null || that.type == null)
+            {
+                typeEquals = type != that.type;
+            }
+            else
+            {
+                typeEquals = !type.equals(that.type);
+            }
+            return typeEquals;
+        }
+        
+        public int hashCode()
+        {
+            int h1 = (name == null) ? 0 : name.hashCode();
+            int h2 = (type == null) ? 0 : type.hashCode();
+            return h1 + h2;
+        }       
+    }
+    
+    public void testHashCodeAndEquals()
+    {
+        Person p1 = new Person();
+        p1.first = new String("John");
+        p1.last = new String("DeRegnaucourt");
+        int a = DeepEquals.deepHashCode(p1);
+        int b = "John".hashCode();
+        int c = "DeRegnaucourt".hashCode();
+        assertTrue(a != b && b != c);
+        
+        Pet pet1 = new Pet();
+        pet1.name = "Eddie";
+        pet1.type = new String("dog");
+        
+        Pet pet2 = new Pet();
+        pet2.name = "Penny";
+        pet2.type = new String("dog");
+        
+        p1.pet = pet1;
+        
+        Person p2 = new Person();
+        p2.first = new String("John");
+        p2.last = new String("DeRegnaucourt");
+        
+        p2.pet = pet2;
+        
+        assertTrue(p1.first != p2.first);  // Ensure that the Strings are not ==
+        
+        assertTrue(!DeepEquals.deepEquals(p1, p2));
+        assertTrue(!p1.equals(p2));
+        
+        p2.pet = pet1;
+        assertTrue(DeepEquals.deepEquals(p1, p2));
+        assertTrue(!p1.equals(p2)); // should be different because it would use Object.equals() which is instance based        
+    }
+
+    public void testCycleHandlingHashCode()
+    {
+        Cycle a = new Cycle();
+        a.value = new String("foo");
+        Cycle b = new Cycle();
+        b.value = new String("bar");
+        Cycle c = new Cycle();
+        c.value = new String("baz");
+        
+        a.next = b;
+        b.next = c;
+        c.next = a;
+        
+        int ha = DeepEquals.deepHashCode(a);
+        int hb = DeepEquals.deepHashCode(b);
+        int hc = DeepEquals.deepHashCode(c);
+        
+        assertTrue(ha == hb && hb == hc);        
+    }
+    
+    public void testCycleHandlingEquals()
+    {
+        Cycle a1 = new Cycle();
+        a1.value = new String("foo");
+        Cycle b1 = new Cycle();
+        b1.value = new String("bar");
+        Cycle c1 = new Cycle();
+        c1.value = new String("baz");
+        
+        a1.next = b1;
+        b1.next = c1;
+        c1.next = a1;
+        
+        Cycle a2 = new Cycle();
+        a2.value = new String("foo");
+        Cycle b2 = new Cycle();
+        b2.value = new String("bar");
+        Cycle c2 = new Cycle();
+        c2.value = new String("baz");
+        
+        a2.next = b2;
+        b2.next = c2;
+        c2.next = a2;
+                
+        assertTrue(DeepEquals.deepEquals(a1, a2));
+        assertTrue(DeepEquals.deepEquals(b1, b2));
+        assertTrue(DeepEquals.deepEquals(c1, c2));
+        assertFalse(DeepEquals.deepEquals(a1, b2));
+        assertFalse(DeepEquals.deepEquals(b1, c2));
+        assertFalse(DeepEquals.deepEquals(c1, a2));
+    }
+    
+    public void testHierarchyCycleEquals()
+    {        
+        FixedHierarchy h1 = new FixedHierarchy();
+        h1.value = new String("root");
+        FixedHierarchy c1 = new FixedHierarchy();
+        c1.value = new String("child1");        
+        FixedHierarchy c2 = new FixedHierarchy();
+        c2.value = new String("child2");
+        
+        h1.child1 = c1;
+        h1.child2 = c2;
+        h1.child3 = c1;
+        
+        FixedHierarchy h2 = new FixedHierarchy();
+        h2.value = new String("root");
+        FixedHierarchy k1 = new FixedHierarchy();
+        k1.value = new String("child1");        
+        FixedHierarchy k2 = new FixedHierarchy();
+        k2.value = new String("child2");
+        
+        h2.child1 = k1;
+        h2.child2 = k2;
+        h2.child3 = k1;
+        
+        assertTrue(DeepEquals.deepEquals(h1, h2));
+    }
+    
+    public void testDeepEquals()
+    {
+        SmartPet smartPet1 = new SmartPet();
+        smartPet1.name = new String("Fido");
+        smartPet1.type = new String("Terrier");
+        
+        SmartPet smartPet2 = new SmartPet();
+        smartPet2.name = new String("Fido");
+        smartPet2.type = new String("Terrier");
+        
+        assertFalse(DeepEquals.deepEquals(smartPet1, smartPet2));   // Only way to get false is if it calls .equals()
+        
+        ArrayClass ac1 = new ArrayClass();
+        ac1.name = new String("Object Array");
+        ac1.items = new Object[] {new String("Hello"), 16, 16L, null, 'c', new Boolean(true), 0.04, new Object[] {"a", 2, 'c' }, new String[] {"larry", "curly", new String("mo")}};
+        ArrayClass ac2 = new ArrayClass();
+        ac2.name = new String("Object Array");
+        ac2.items = new Object[] {new String("Hello"), 16, 16L, null, 'c', Boolean.TRUE, new Double(0.04), new Object[] {"a", 2, 'c' }, new String[] {"larry", new String("curly"), "mo"}};
+        
+        assertTrue(DeepEquals.deepEquals(ac1, ac2));
+    }
+    
+    public void testBasicEquals()
+    {
+        String one = new String("One");
+        String two = new String("Two");
+        String a = new String("One");
+        
+        assertFalse(DeepEquals.deepEquals(one, two));
+        assertTrue(DeepEquals.deepEquals(one, a));
+        
+        Double x = 1.04;
+        Double y = 1.039999;
+        Double z = 1.04;
+        
+        assertFalse(DeepEquals.deepEquals(x, y));
+        assertTrue(DeepEquals.deepEquals(x, z));
+    }
+    
+    public void testBasicHashCode()
+    {
+        String one = new String("One");
+        assertTrue(DeepEquals.deepHashCode(one) == one.hashCode());
+        
+        Double pi = 3.14159;
+        assertTrue(DeepEquals.deepHashCode(pi) == pi.hashCode());
+        
+        Calendar c = Calendar.getInstance();
+        assertTrue(DeepEquals.deepHashCode(c) == c.hashCode());
+        
+        Date date = new Date();
+        assertTrue(DeepEquals.deepHashCode(date) == date.hashCode());
+    }
+}
